f8a5e180a1d65cbdbd9450ca37fca1b8d538ac79
MHS Technologies <mhstechcompany@gmail.com>
2022-05-31 23:14:13 +1200

    It includes files to create the dabatase, create the collection, insert data, find one data, and sort data

--- db_sortbyname.js
+++ db_sortbyname.js
@@ -6,15 +6,15 @@ dotenv.config();
 const url = process.env.URL;
 const database = process.env.DATABASE;
 
-
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   
   const dbo = db.db(database);
+  const mysort = { name: 1 };
   
-  dbo.createCollection("products", function(err, res) {
+  dbo.collection("products").find().sort(mysort).toArray(function(err, result) {
     if (err) throw err;
-    console.log("Collection created!");
+    console.log(result);
     db.close();
   });
 }); 
